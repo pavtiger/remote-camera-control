499b48d33a2b12d16e440cd64639912abe0cd930
Paul Artushkov <pavTiger@gmail.com>
2023-05-02 23:45:13 +0300

    Initial arrow controls

--- server.py
+++ server.py
@@ -23,6 +23,10 @@ sio.attach(app)
 wCap = cv2.VideoCapture(0)
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
+# Servo options
+pos = (0, 0)
+delta = (0, 0)
+
 
 # Static files server
 async def index(request):
@@ -33,7 +37,25 @@ async def index(request):
 app.router.add_static('/static', 'static')
 app.router.add_get('/', index)
 
-async def send_ping():
+
+@sio.on('up')
+async def up(sid):
+    print("up")
+
+@sio.on('down')
+async def down(sid):
+    print("down")
+
+@sio.on('left')
+async def left(sid):
+    print("left")
+
+@sio.on('right')
+async def right(sid):
+    print("right")
+
+
+async def send_images():
     while True:
         ret, frame = wCap.read()
         frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
@@ -44,7 +66,7 @@ async def send_ping():
         await sio.sleep(0.1)
 
 async def init_app():
-    sio.start_background_task(send_ping)
+    sio.start_background_task(send_images)
     return app
 
 
