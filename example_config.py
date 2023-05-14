b3cebad0511cf070a74d2107c14ca804bbc6386d
Paul Artushkov <pavtiger@gmail.com>
2023-05-15 00:42:39 +0100

    Add IP override option

--- example_config.py
+++ example_config.py
@@ -7,4 +7,5 @@ resolution = [1280, 720]  # Width, height
 step = [10, 10]  # Step distance for each moment (constant)
 spill_threshold = 500  # Amount of time that is counted as error in http requests (ms)
 control_mode = "drag"  # Either "joystick" or "drag"
+server_ip_override = ""  # Override ip that is used on client's side to connect to remote server (leave empty not to engage or in format "http://x.x.x.x:port")
 
